2615f54b1e207e19cd80fa380b8ca8cf415ee40d
Bot hub offical <76042379+BotHub-1@users.noreply.github.com>
2021-01-04 18:36:07 +0530

    Update misc.py
    mnx

--- cinderella/modules/helper_funcs/misc.py
+++ cinderella/modules/helper_funcs/misc.py
@@ -69,7 +69,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⪻", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🍁Home🍁", callback_data="bot_start"),
+                EqInlineKeyboardButton("🎀Home🎀", callback_data="bot_start"),
              EqInlineKeyboardButton("⪼", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
